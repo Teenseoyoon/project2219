7091e252dabf2b7c3b3d486ee61f0d00dd7ef1a1
Teenseoyoon <0227seoyoon@gmail.com>
2025-08-06 10:19:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -330,7 +330,7 @@ else:
                     hp, atk = 800, 200
                 else:
                     hp, atk = 300, 1400
-                add_user(name, password, job, hp, atk)
+                add_user(name, password, job, hp, atk, stage)
                 st.success("회원가입 완료! 로그인해주세요.")
 
     elif mode == "로그인":
